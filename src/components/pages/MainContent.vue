3cfe6f92376b92ee3c9d5cc28eea3387dfd5c2ee
toom1996 <1023150697@qq.com>
2024-01-01 23:13:00 +0800

    update

--- src/components/pages/MainContent.vue
+++ src/components/pages/MainContent.vue
@@ -2,27 +2,193 @@
   <div>
     <div class="wrapper pt-60">
       <ul class="flex">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        <li
+          v-for="(item, index) in dataProvider"
+          :style="computedWrapperStyle(index)"
+        >
+          <div class="cover-wrapper">
+            <div
+              aria-label="Social Media Design for OpusClip | Social Media Post"
+              class="ProjectCoverNeue-root-B1h ProjectCoverNeue-statsVisible-Sg2 ProjectCover-cover-ucA qa-search-project-item"
+            >
+              <div
+                class="Cover-cover-gDM ProjectCoverNeue-cover-X3S e2e-ProjectCoverNeue js-project-cover e2e-ProjectCoverNeue-cover ProjectCoverNeue-statsVisible-Sg2 ProjectCoverNeue-loaded-sa9"
+              >
+                <div
+                  class="Cover-wrapper-H_F ProjectCoverNeue-wrapper-Oqj e2e-ProjectCoverNeue-wrapper"
+                >
+                  <div class="Cover-content-yv3">
+                    <div
+                      style="background-color: rgb(22, 20, 21)"
+                      class="DominantColor-dominantColor-iv4 ProjectCoverNeue-dominantColor-oi6"
+                    ></div>
+                    <picture class="ProjectCoverNeue-picture-NuE">
+                      <source
+                        srcset="
+                          https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png 808w,
+                          https://mir-s3-cdn-cf.behance.net/projects/404_webp/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png     404w,
+                          https://mir-s3-cdn-cf.behance.net/projects/202_webp/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png     202w,
+                          https://mir-s3-cdn-cf.behance.net/projects/230_webp/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png     230w,
+                          https://mir-s3-cdn-cf.behance.net/projects/115_webp/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png     115w
+                        "
+                        type="image/webp"
+                      />
+                      <source
+                        srcset="
+                          https://mir-s3-cdn-cf.behance.net/projects/115/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png     115w,
+                          https://mir-s3-cdn-cf.behance.net/projects/202/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png     202w,
+                          https://mir-s3-cdn-cf.behance.net/projects/230/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png     230w,
+                          https://mir-s3-cdn-cf.behance.net/projects/404/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png     404w,
+                          https://mir-s3-cdn-cf.behance.net/projects/max_808/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png 808w
+                        "
+                        type="image/png"
+                      />
+                      <img
+                        sizes="404px"
+                        src="https://mir-s3-cdn-cf.behance.net/projects/404/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png"
+                        class="ProjectCoverNeue-image-TFB js-cover-image ProjectCoverNeue-imageHoverOverlay-m1P"
+                        alt="Social Media Design&nbsp;for OpusClip | Social Media Post"
+                        loading="lazy"
+                        style=""
+                      />
+                    </picture>
+                    <div
+                      class="ProjectCoverNeue-controlsAndPrivacyContainer-w3G ProjectCoverNeue-containerFitContent-uQq"
+                    >
+                      <div
+                        class="ProjectCoverNeue-assetAndSubscribedContainer-TI7"
+                      ></div>
+                      <div class="ProjectCoverNeue-addToMoodboardAction-ye4">
+                        <button
+                          class="Btn-button-CqT Btn-inverted-GDL Btn-small-lgf ProjectCoverNeue-addToMoodboardBtn-G7u"
+                          type="button"
+                          target="_self"
+                          role="button"
+                          aria-label="添加到情绪板 - Social Media Design&nbsp;for OpusClip | Social Media Post"
+                        >
+                          <div class="Btn-labelWrapper-_Re">
+                            <div class="Btn-icon-BDP Btn-leading-gb0">
+                              <svg
+                                class="Collection-icon-mXx ProjectCoverNeue-moodboardIcon-UrT"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="17"
+                                height="17"
+                                viewBox="0 0 17 17"
+                              >
+                                <path
+                                  d="M5.69,3.395,7.97,5.487h6.217V7.579H2.858V3.743a.351.351,0,0,1,.354-.349ZM5.69,2H3.212a1.757,1.757,0,0,0-1.77,1.743V7.579h-1a.356.356,0,0,0-.27.123A.345.345,0,0,0,.1,7.985L1.336,14.68a.705.705,0,0,0,.7.572H15.012a.705.705,0,0,0,.7-.572L16.95,7.985a.345.345,0,0,0-.079-.283.356.356,0,0,0-.27-.123h-1V4.789a.7.7,0,0,0-.708-.7H8.523L6.859,2.514A1.657,1.657,0,0,0,5.69,2Z"
+                                  transform="translate(-0.023 -0.5)"
+                                ></path>
+                              </svg>
+                            </div>
+                            <div class="Btn-label-QJi e2e-Btn-label">保存</div>
+                          </div>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="Cover-overlay-r1A Cover-showOnHover-oZ2">
+                    <a
+                      href="/gallery/184704669/Social-Media-Designfor-OpusClip-Social-Media-Post?tracking_source=search_projects&amp;l=0"
+                      class="ProjectCoverNeue-coverLink-U39 e2e-ProjectCoverNeue-link"
+                      title="项目的链接 - Social Media Design&nbsp;for OpusClip | Social Media Post"
+                    ></a>
+                  </div>
+                </div>
+                <div class="ProjectCover-details-ute">
+                  <div class="ProjectCover-info-sxd">
+                    <span class="TitleOwner-limitHeight-_cX">
+                      <a
+                        href="https://www.behance.net/gallery/184704669/Social-Media-Designfor-OpusClip-Social-Media-Post?tracking_source=search_projects"
+                        class="Title-title-lpJ e2e-Title-owner Title-mediumTitle-OeE TitleOwner-title-H7y"
+                        tabindex="0"
+                        >Social Media Design&nbsp;for OpusClip | Social Media
+                        post</a
+                      >
+                      <div
+                        class="Owners-root-qU7 Owners-dark-BYv Owners-overflowText-C9U TitleOwner-owner-I52 TitleOwner-mediumOwner-eDx"
+                      >
+                        <div class="Owners-miniprofileActivatorContainer-k0t">
+                          <div aria-haspopup="true">
+                            <div
+                              aria-describedby="f2fcc371-ad91-4dfe-a1b3-7426b3b1952a"
+                              class="Popover-activator-M8N Owners-miniprofileActivator-dc_ Miniprofile-activator-dDq"
+                            >
+                              <span
+                                ><a
+                                  href="https://www.behance.net/mahdyhasanhridoy?tracking_source=search_projects"
+                                  class="Owners-owner-EEG e2e-Owner-user-link"
+                                  >Mahdy Hasan Hridoy</a
+                                ></span
+                              >
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </span>
+                  </div>
+                  <div class="ProjectCover-stats-QLg">
+                    <div class="Stats-stats-Q1s">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        viewBox="0.5 0.5 16 16"
+                        class="Appreciations-icon-Z4i ProjectCover-icon-QsA ProjectCover-appreciations-hIS"
+                      >
+                        <path fill="none" d="M.5.5h16v16H.5z"></path>
+                        <path
+                          d="M.5 7.5h3v8h-3zM7.207 15.207c.193.19.425.29.677.293H12c.256 0 .512-.098.707-.293l2.5-2.5c.19-.19.288-.457.293-.707V8.5c0-.553-.445-1-1-1h-5L11 5s.5-.792.5-1.5v-1c0-.553-.447-1-1-1l-1 2-4 4v6l1.707 1.707z"
+                        ></path>
+                      </svg>
+                      <span title="89">89</span
+                      ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        class="ProjectCover-icon-QsA ProjectCover-views-Euf"
+                      >
+                        <path
+                          d="M8.5 3.5c-5 0-8 5-8 5s3 5 8 5 8-5 8-5-3-5-8-5zm0 7c-1.105 0-2-.896-2-2 0-1.106.895-2 2-2 1.104 0 2 .894 2 2 0 1.104-.896 2-2 2z"
+                        ></path>
+                      </svg>
+                      <span title="647">647</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
   </div>
 </template>
 <script setup>
-import { ref, onMounted, reactive } from 'vue'
-const dataProvider = [
+import { ref, onMounted, reactive, computed } from "vue";
+const count = ref(-1);
+const dataProvider = reactive([
+  {
+    title: "123",
+  },
+  {
+    title: "123",
+  },
+  {
+    title: "123",
+  },
+]);
 
-];
+const computedWrapperStyle = (index) => {
+  console.log(index);
+  return {
+    color: "blue",
+    transform: "translate(" + index * 100 + "%, 0%)",
+  };
+};
 onMounted(() => {
-const dataProvider = reactive({
-    
-})
-//   setLoginState()
-  console.log(88888)
-  console.log(document.cookie)
-
-
+  //   setLoginState()
   // 向给定ID的用户发起请求
   // axios.post('http://localhost:8080/api/v1/get-user-info')
   //   .then(function (response) {
@@ -36,7 +202,5 @@ const dataProvider = reactive({
   //   .then(function () {
   //     // 总是会执行
   //   });
-})
-
-console.log('ppp')
+});
 </script>
