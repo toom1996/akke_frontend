84a49ed9e4b0fcba67e466075e489420c8bb47c0
toom1996 <1023150697@qq.com>
2024-01-03 20:20:24 +0800

    update

--- src/components/pages/MainContent.vue
+++ src/components/pages/MainContent.vue
@@ -1,26 +1,18 @@
 <template>
-  <div style="max-width: calc(1980px + 120px);padding:0px 60px">
+  <div style="max-width: calc(1980px + 120px);padding:0px 60px;margin-left: auto;margin-right: auto;">
     <div class="wrapper pt-60">
-      <ul class="grid grid-cols-4 gap-5">
+      <ul class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         <li v-for="(item, index) in dataProvider">
           <div class="cover-wrapper">
-            <div aria-label="Social Media Design for OpusClip | Social Media Post" class="ProjectCoverNeue-root-B1h ProjectCoverNeue-statsVisible-Sg2 ProjectCover-cover-ucA qa-search-project-item"
-            >
-              <div
-                class="Cover-cover-gDM ProjectCoverNeue-cover-X3S e2e-ProjectCoverNeue js-project-cover e2e-ProjectCoverNeue-cover ProjectCoverNeue-statsVisible-Sg2 ProjectCoverNeue-loaded-sa9"
-              >
-                <div
-                  class="cover-container"
-                >
-                  <div class="Cover-content-yv3">
-                    <div
-                      style="background-color: rgb(22, 20, 21)"
-                      class="DominantColor-dominantColor-iv4 ProjectCoverNeue-dominantColor-oi6"
-                    ></div>
-                    <picture class="ProjectCoverNeue-picture-NuE">
+            <div :aria-label="item.title" class="">
+              <div class="">
+                <div class="cover-container relative">
+                  <div class="cover-content ">
+                    <!-- <div style="background-color: rgb(22, 20, 21)" class="cover-mask absolute w-full bottom-0 left-0 top-0 right-0"></div> -->
+                    <picture class="ProjectCoverNeue-picture-NuE h-full w-full absolute top-0">
                       <source
                         srcset="
-                        https://imgwf2.pop-fashion.com/upload/flash_report/2023/2023122810/ANAREPORT_658cda073b90c_9728.jpg 808w,
+                        https://i2.hdslb.com/bfs/archive/706cf1836492ad5a4299a4ff3707f825ceac93f7.jpg@672w_378h_1c_!web-home-common-cover.webp 808w,
                           https://mir-s3-cdn-cf.behance.net/projects/404_webp/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png 404w,
                           https://mir-s3-cdn-cf.behance.net/projects/202_webp/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png 202w,
                           https://mir-s3-cdn-cf.behance.net/projects/230_webp/dde47c184704669.Y3JvcCwxNTM0LDEyMDAsMzQsMA.png 230w,
@@ -41,19 +33,12 @@
                       <img
                         sizes="404px"
                         :src="item.cover"
-                        class="ProjectCoverNeue-image-TFB js-cover-image ProjectCoverNeue-imageHoverOverlay-m1P"
+                        class="h-full w-full object-cover"
                         alt="Social Media Design&nbsp;for OpusClip | Social Media Post"
                         loading="lazy"
                         style=""
                       />
                     </picture>
-                  </div>
-                  <div class="Cover-overlay-r1A Cover-showOnHover-oZ2">
-                    <a
-                      href="/gallery/184704669/Social-Media-Designfor-OpusClip-Social-Media-Post?tracking_source=search_projects&amp;l=0"
-                      class="ProjectCoverNeue-coverLink-U39 e2e-ProjectCoverNeue-link"
-                      title="项目的链接 - Social Media Design&nbsp;for OpusClip | Social Media Post"
-                    ></a>
                   </div>
                 </div>
                 <div class="ProjectCover-details-ute flex">
@@ -61,11 +46,10 @@
                     <span class="TitleOwner-limitHeight-_cX">
                       <a
                         href="https://www.behance.net/gallery/184704669/Social-Media-Designfor-OpusClip-Social-Media-Post?tracking_source=search_projects"
-                        class="Title-title-lpJ e2e-Title-owner Title-mediumTitle-OeE TitleOwner-title-H7y"
+                        class="text-sm font-bold"
                         tabindex="0"
                         >{{ item.title }} | Social Media
-                        post</a
-                      >
+                        </a>
                       <div
                         class="Owners-root-qU7 Owners-dark-BYv Owners-overflowText-C9U TitleOwner-owner-I52 TitleOwner-mediumOwner-eDx"
                       >
@@ -75,13 +59,11 @@
                               aria-describedby="f2fcc371-ad91-4dfe-a1b3-7426b3b1952a"
                               class="Popover-activator-M8N Owners-miniprofileActivator-dc_ Miniprofile-activator-dDq"
                             >
-                              <span
-                                ><a
-                                  href="https://www.behance.net/mahdyhasanhridoy?tracking_source=search_projects"
-                                  class="Owners-owner-EEG e2e-Owner-user-link"
-                                  >Mahdy Hasan Hridoy</a
-                                ></span
-                              >
+                              <span>
+                                <a href="https://www.behance.net/mahdyhasanhridoy?tracking_source=search_projects" class="text-xs">
+                                  Mahdy Hasan Hridoy
+                                </a>
+                              </span>
                             </div>
                           </div>
                         </div>
@@ -90,9 +72,9 @@
                   </div>
                   <div class="detail-wrapper">
                     <div class="detail-content">
-                      <MdiLightEye></MdiLightEye>
-                      <span title="89">89</span
-                      ><MaterialSymbolsAddPhotoAlternateOutline />
+                      <MdiLightEye class="eye" />
+                      <span title="89">89</span>
+                      <MaterialSymbolsAddPhotoAlternateOutline class="img" />
                       <span title="647">647</span>
                     </div>
                   </div>
@@ -111,14 +93,25 @@
 }
 
 .detail-content {
-  @apply font-bold flex items-center;
+  @apply font-bold flex items-center text-xs;
+}
+
+.cover-content::before {
+  display: block;
+  content: "123";
+  width: 100%;
+  padding-top: 56.25%;
+  background-color: rgb(241, 241, 241)
+}
+
+@media screen {
+  
 }
 </style>
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted, reactive, computed } from "vue";
-import MdiLightEye from '../../assets/icons/MdiLightEye.vue';
+import MdiLightEye from '@/assets/icons/MdiLightEye.vue';
 import MaterialSymbolsAddPhotoAlternateOutline from '../../assets/icons/MaterialSymbolsAddPhotoAlternateOutline.vue'
-const count = ref(-1);
 const dataProvider = reactive([
   {
     title: "内衣--女装12月TOP热榜",
@@ -142,11 +135,9 @@ const dataProvider = reactive([
   },
 ]);
 
-const computedWrapperStyle = (index) => {
-  console.log(index);
+const computedWrapperStyle = () => {
   return {
     color: "blue",
-    transform: "translate(" + index * 100 + "%, 0%)",
   };
 };
 onMounted(() => {
